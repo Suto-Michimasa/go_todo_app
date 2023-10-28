95987d99215d1a8f680702694517481eb142df42
Suto <sutou1335michimasa@gmail.com>
2023-10-28 21:20:52 +0900

    create db

--- go.mod
+++ go.mod
@@ -4,5 +4,6 @@ go 1.21.0
 
 require (
 	github.com/go-ini/ini v1.67.0 // indirect
+	github.com/mattn/go-sqlite3 v1.14.17 // indirect
 	gopkg.in/go-ini/ini.v1 v1.67.0 // indirect
 )
--- main.go
+++ main.go
@@ -2,15 +2,14 @@ package main
 
 import (
 	"fmt"
-	"go_todo_app/config"
-	"log"
+	"go_todo_app/app/models"
 )
 
 func main() {
-	fmt.Println(config.Config.Port)
-	fmt.Println(config.Config.SQLDriver)
-	fmt.Println(config.Config.DbName)
-	fmt.Println(config.Config.LogFile)
+	// fmt.Println(config.Config.Port)
+	// fmt.Println(config.Config.SQLDriver)
+	// fmt.Println(config.Config.DbName)
+	// fmt.Println(config.Config.LogFile)
 
-	log.Println("test")
+	fmt.Println(models.Db)
 }
