ac80ce46d0b72d7a90fb6cbc5ac2bb7f7990ec35
Suto <sutou1335michimasa@gmail.com>
2023-10-29 14:05:49 +0900

    setting server

--- main.go
+++ main.go
@@ -2,49 +2,12 @@ package main
 
 import (
 	"fmt"
+	"go_todo_app/app/controllers"
 	"go_todo_app/app/models"
 )
 
 func main() {
 	fmt.Println(models.Db)
 
-	// u := &models.User{}
-	// u.Name = "test2"
-	// u.Email = "test2@example.com"
-	// u.Password = "testtest"
-	// fmt.Println(u)
-	// u.CreateUser()
-
-	// u, _ := models.GetUser(1)
-	// fmt.Println(u)
-
-	// u.Name = "Test2"
-	// u.Email = "test2@example.com"
-	// u.UpdateUser()
-	// u, _ = models.GetUser(1)
-	// fmt.Println(u)
-
-	// u.DeleteUser()
-	// user, _ := models.GetUser(2)
-	// user.CreateTodo("First Todo")
-	// user, _ := models.GetUser(3)
-	// user.CreateTodo("Third Todo")
-
-	// todos, _ := models.GetTodos()
-	// for _, v := range todos {
-	// 	fmt.Println(v)
-	// }
-
-	// user2, _ := models.GetUser(3)
-	// t, _ := user2.GetTodosByUser()
-	// for _, v := range t {
-	// 	fmt.Println(v)
-	// }
-
-	// t, _ := models.GetTodo(1)
-	// t.Content = "Update Todo"
-	// t.UpdateTodo()
-
-	t, _ := models.GetTodo(4)
-	t.DeleteTodo()
+	controllers.StartMainServer()
 }
